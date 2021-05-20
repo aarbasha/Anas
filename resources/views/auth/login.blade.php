88976ffa806071e263bc4f43d03c612de8543173
Anas Arbasha <anas.arbasha.sy@gmail.com>
2021-05-21 01:22:15 +0300

    Create Web Applcation is Personal

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Login') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="form-group row">
+
+
+                                <div class="col-md-6 offset-md-3">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                        name="email" value="{{ old('email') }}" required autocomplete="off" autofocus
+                                        placeholder="Email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+
+
+                                <div class="col-md-6 offset-md-3">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="off" placeholder="Password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-3">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                            {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-3">
+                                    <button type="submit" class="btn btn-lg btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                            <hr class="" style="background: rgba(83, 82, 82, 0.565)">
+                            <p class="text-center mt-3" style="font-weight:bold">OR</p>
+
+                            <div class="d-flex  justify-content-center mt-4 ">
+
+                                <a href="{{ route('login.github')}}" class="m-2 btn  btn-lg"
+                                    style="background:black;color:white;padding:10px 30px;border-radius:20px;border:none;">Github</a>
+                                <a href="{{ route('login.facebook')}}" class="m-2 btn  btn-lg"
+                                    style="background:rgb(0, 31, 236);color:white;padding:10px 30px;border-radius:20px;border:none;">Facebook</a>
+                                <a href="{{ route('login.google')}}" class="m-2 btn  btn-lg"
+                                    style="background:rgb(186, 0, 0);color:white;padding:10px 30px;border-radius:20px;border:none;">Google</a>
+
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\loginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//GOOGLE LOGIN
+Route::get("login/google" , [loginController::class,"redirectToGoogle"])->name("login.google");
+Route::get("login/google/callback" , [loginController::class,"getDataGoogleCallback"]);
+
+//FACEBOOK LOGIN
+Route::get("login/facebook" , [loginController::class,"redirectToFacebook"])->name("login.facebook");
+Route::get("login/facebook/callback" , [loginController::class,"getDataFacebookCallback"]);
+
+//GITHUB LOGIN
+Route::get("login/github" , [loginController::class,"redirectToGithub"])->name("login.github");
+Route::get("login/github/callback" , [loginController::class,"getDataGithubCallback"]);
